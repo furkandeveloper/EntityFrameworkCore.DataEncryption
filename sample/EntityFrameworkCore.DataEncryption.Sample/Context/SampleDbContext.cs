e0b208dc5e8c66096b36bb359eb1dcf4b9704c62
furkandeveloper <furkan.dvlp@gmail.com>
2022-05-15 00:19:20 +0300

    🔨 Implement Sample Db Context

---
+++ sample/EntityFrameworkCore.DataEncryption.Sample/Context/SampleDbContext.cs
@@ -0,0 +1,56 @@
+﻿using EntityFrameworkCore.DataEncryption.Conversions;
+using EntityFrameworkCore.DataEncryption.Sample.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.DataEncryption.Sample.Context;
+
+/// <summary>
+/// Sample Db Context
+/// </summary>
+public class SampleDbContext : DbContext
+{
+    /// <summary>
+    /// protected ctor
+    /// </summary>
+    protected SampleDbContext()
+    {
+    }
+
+    /// <summary>
+    /// public ctor
+    /// </summary>
+    /// <param name="options">
+    /// DbContext Options object. <see cref="DbContextOptions{TContext}"/>
+    /// </param>
+    public SampleDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public virtual DbSet<Author> Authors { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity
+                .Property(p => p.Id)
+                .UseIdentityColumn();
+
+            entity
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(2048);
+            
+            entity
+                .Property(p => p.Surname)
+                .IsRequired()
+                .HasMaxLength(2048);
+
+            entity
+                .Property(p => p.Phone)
+                .IsRequired()
+                .HasConversion(new EncryptValueConverter("89acMXSBpuEBDWHZ"));
+        });
+        base.OnModelCreating(modelBuilder);
+    }
+}
