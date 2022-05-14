649dcd6ad515ea3138db6364748053e9358944da
furkandeveloper <furkan.dvlp@gmail.com>
2022-05-15 00:20:12 +0300

    🔨 Configure init settings

--- sample/EntityFrameworkCore.DataEncryption.Sample/Program.cs
+++ sample/EntityFrameworkCore.DataEncryption.Sample/Program.cs
@@ -1,6 +1,28 @@
+using System.Text.Json.Serialization;
+using EntityFrameworkCore.DataEncryption.Sample.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+builder.Services.AddDbContext<SampleDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapControllers();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
