680fc3f164893a0ddc32b65500df7b36b9cf41c5
furkandeveloper <furkan.dvlp@gmail.com>
2022-05-15 00:03:45 +0300

    🔨 Add Author entity

---
+++ sample/EntityFrameworkCore.DataEncryption.Sample/Entities/Author.cs
@@ -0,0 +1,27 @@
+﻿namespace EntityFrameworkCore.DataEncryption.Sample.Entities;
+
+/// <summary>
+/// Author Entity
+/// </summary>
+public class Author
+{
+    /// <summary>
+    /// PK
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Name of Author
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Surname of Author
+    /// </summary>
+    public string Surname { get; set; }
+
+    /// <summary>
+    /// Phone of Author
+    /// </summary>
+    public string Phone { get; set; }
+}
