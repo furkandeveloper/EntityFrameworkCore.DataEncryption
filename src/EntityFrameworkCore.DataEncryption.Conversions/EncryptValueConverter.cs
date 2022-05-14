6983b4679006cfeded768ddc1bb4893173482dc5
furkandeveloper <furkan.dvlp@gmail.com>
2022-05-14 23:37:10 +0300

    🔨 Add Encrypt Value Converter for conversions

---
+++ src/EntityFrameworkCore.DataEncryption.Conversions/EncryptValueConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+using EntityFrameworkCore.DataEncryption.Core;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace EntityFrameworkCore.DataEncryption.Conversions;
+
+/// <summary>
+/// Encrypt Value Converter  
+/// </summary>
+public class EncryptValueConverter : ValueConverter<string, string>
+{
+    private static string _password;
+
+    public EncryptValueConverter(string key, ConverterMappingHints mappingHints = default)
+        : base(EncryptExpr, DecryptExpr, mappingHints)
+    {
+        _password = key;
+    }
+
+    // from
+    /// <summary>
+    /// From Expression
+    /// </summary>
+    private static Expression<Func<string, string>> DecryptExpr => prop => Cipher.Decrypt(prop, _password);
+
+    // to
+    /// <summary>
+    /// To Expression
+    /// </summary>
+    private static Expression<Func<string, string>> EncryptExpr => prop => Cipher.Encrypt(prop, _password);
+}
