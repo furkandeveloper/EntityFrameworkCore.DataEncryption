456cff2d96c87755be913404e6159fa50b7687e7
furkandeveloper <furkan.dvlp@gmail.com>
2022-05-15 22:21:49 +0300

    🔨 Implement Author endpoints

---
+++ sample/EntityFrameworkCore.DataEncryption.Sample/Controllers/AuthorController.cs
@@ -0,0 +1,31 @@
+﻿using EntityFrameworkCore.DataEncryption.Sample.Context;
+using EntityFrameworkCore.DataEncryption.Sample.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkCore.DataEncryption.Sample.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthorController : ControllerBase
+{
+    private readonly SampleDbContext _context;
+
+    public AuthorController(SampleDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet(Name = "Filter")]
+    public Task<IActionResult> FilterAsync([FromQuery]string phone)
+    {
+        return Task.FromResult<IActionResult>(Ok(_context.Authors.Where(a=>a.Phone == phone).ToList()));
+    }
+
+    [HttpPost(Name = "Insert")]
+    public async Task<IActionResult> InsertAsync([FromBody]Author author)
+    {
+        await _context.Authors.AddAsync(author);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
